f9f181a49d110b11a7e1fa4abd5cd3d5ce9bcf99
Bonington <yikun.chen@163.com>
2024-07-29 09:01:31 +0800

    Merge in 3D ray tracing in a 2D space using a raw way.

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -1,9 +1,13 @@
+
+#include <dinput.h>
+
 #include "Walnut/Application.h"
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
+
+#include "Renderer.h"
 
 using namespace Walnut;
 
@@ -26,10 +30,12 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if (m_Image)
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
 			ImGui::Image(
-			m_Image->GetDescriptorSet(),
-			{ (float)m_Image->GetWidth(), (float)m_Image->GetHeight() }
+				image->GetDescriptorSet(),
+			{ (float)image->GetWidth(), (float)image->GetHeight() },
+				ImVec2(0, 1), ImVec2(1, 0)
 			);
 
 		ImGui::End();
@@ -42,30 +48,14 @@ public:
 	{
 		Timer timer;
 
-		if (
-			!m_Image 
-			|| m_ViewportWidth != m_Image->GetWidth() 
-			|| m_ViewportHeight != m_Image->GetHeight()
-			)
-		{
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
 
-			for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
-			{
-				m_ImageData[i] = Random::UInt();
-				m_ImageData[i] |= 0xff000000;
-			}
-
-			m_Image->SetData(m_ImageData);
-		}
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr;
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 	float m_LastRenderTime = 0.0f;
